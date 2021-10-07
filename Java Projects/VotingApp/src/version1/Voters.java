2884a0a9c4f983435f14bc1722fc8d9484839bfb
Mashudu Ndou <mashudun@digititan.co.za>
2021-10-07 18:55:12 +0200

    java applications

---
+++ Java Projects/VotingApp/src/version1/Voters.java
@@ -0,0 +1,71 @@
+package version1;
+
+/* Mashudu 
+Patricia
+Judith
+mamosa */
+
+
+public class Voters {
+	
+	private String votersName;
+	String votersSurname;
+	int votersId;
+	int voterAge;
+	int votersRef;
+	
+	public Voters(String votersName, String votersSurname, int votersId, int voterAge, int votersRef){
+        this.votersName=votersName;
+        this.votersSurname=votersSurname;
+        this.votersId=votersId;
+        this.voterAge=voterAge;
+        this.votersRef = votersRef;
+	}
+	
+	
+	 public String getvotersName() {
+	        return votersName;
+	    }
+
+	    public void setvotersName(String votersName) {
+	        this.votersName = votersName;
+	    }
+	    
+		 public String getvotersSurname() {
+		        return votersSurname;
+		    }
+
+		    public void setvotersSurname(String votersSurname) {
+		        this.votersSurname = votersSurname;
+		    }
+
+	    public int getvotersId() {
+	        return votersId;
+	    }
+
+	    public void setvotersId(int votersId) {
+	        this.votersId = votersId;
+	    }
+
+	    public int getvoterAge() {
+	        return voterAge;
+	    }
+
+	    public void setvoterAge(int voterAge) {
+	        this.voterAge = voterAge;
+	    }
+	    
+	    public int getvotersRef() {
+	        return votersRef;
+	    }
+
+	    public void setvotersRef(int votersRef) {
+	        this.voterAge = votersRef;
+	    }
+
+	
+	    public void printVoterInfo(){
+
+	        System.out.println(" Voter Name: " + votersName + " "+ votersSurname + "\n Voters Id: " + votersId + "\n Age :" + voterAge + votersId + "\n Reference :" + votersRef);
+	    }
+}
